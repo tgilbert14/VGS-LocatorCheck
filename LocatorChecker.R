86f6b15b1b0e1b1ff23e081bd3b2eb817ce3eed0
Timothy Gilbert <tsgilbert35@hotmail.com>
2024-04-18 12:22:04 -0700

    finised code to produce QA/QC file

--- LocatorChecker.R
+++ LocatorChecker.R
@@ -6,24 +6,63 @@ setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
 library(sf)
 library(sp)
 library(rgdal)
+library(uuid)
+library(tidyverse)
+library(DBI)
+library(RSQLite)
+library(stringr)
+library(readxl)
+library(openxlsx)
+
+## SQL local Connection info from R to local VGS5 (VGS50.db)
+db_loc <- "C:/ProgramData/VGSData/VGS50.db"
+mydb <- dbConnect(RSQLite::SQLite(), dbname = db_loc)
+
+## get locator data from VGS .db
+query_get_loctors <- "Select SiteID, DDLong as lon, DDLat as lat, Ancestry from locator
+inner join site on site.PK_Site = Locator.FK_Site
+inner join AncestryCombinedPath ON AncestryCombinedPath.PK_Site = Site.PK_Site
+order by Ancestry, SiteID"
+
+locator_data<- dbGetQuery(mydb, query_get_loctors)
+# end VGS connection
+dbDisconnect(mydb)
 
 # read in shape file
 shapefile_pasture<- rgdal::readOGR(dsn="E:/VGS - Deathstar II/GIS II/S_USA.Pasture", "S_USA.Pasture")
-pasture<- as.data.frame(shapefile_pasture)
+#pasture<- as.data.frame(shapefile_pasture)
 
-#View(allotment)
-#View(pasture)
+# Convert lat and lon to numeric
+locator_data$lat <- as.numeric(locator_data$lat)
+locator_data$lon <- as.numeric(locator_data$lon)
 
-# create a sample point / check sampling point
-point <- data.frame(lat = 31.36, lon = -111.06)
-coordinates(point) <- c("lon", "lat")
+# Create a point data frame
+point <- locator_data[, c("lon", "lat")]
+coordinates(point) <- ~lon+lat
 
-#proj4string(point) <- proj4string(shapefile_allotment)
-# find what row the point falls in
-#allotment_result <- over(point, shapefile_allotment)
-
+# Set the projection of point to match shapefile_pasture
 proj4string(point) <- proj4string(shapefile_pasture)
-# find what row the point falls in
+
+# Find what row each point falls in
 pasture_result <- over(point, shapefile_pasture)
 
-View(pasture_result)
+# Combine the results with the original data
+results_table <- cbind(locator_data, pasture_result)
+
+# create excel sheet to show results
+#View(results_table)
+write.xlsx(results_table, paste0("results/placingLocators_by_Shapefile_",Sys.Date(),".xlsx"))
+
+
+
+new_data<- results_table %>% 
+  mutate("PredictedName" = paste0( substr(MANAGING_O,1,2),"-",
+                                   substr(MANAGING_O,3,4),"-",
+                                   substr(MANAGING_O,5,6),"-",
+                                   ALLOTMENT1,"-",PASTURE_NU,"-KA#"))
+
+site_name_compare<- new_data %>% 
+  select(SiteID, PredictedName, Ancestry)
+
+write.xlsx(site_name_compare, paste0("results/Projected_SiteName_Comparison_",Sys.Date(),".xlsx"))
+
