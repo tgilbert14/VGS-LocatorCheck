0b4e779376327e25643cdbad1fde22e4ea1de7b5
Timothy Gilbert <tsgilbert35@hotmail.com>
2024-04-19 09:35:35 -0700

    Updating selected columns for excel export

--- LocatorChecker.R
+++ LocatorChecker.R
@@ -18,18 +18,18 @@ library(openxlsx)
 db_loc <- "C:/ProgramData/VGSData/VGS50.db"
 mydb <- dbConnect(RSQLite::SQLite(), dbname = db_loc)
 
-## get locator data from VGS .db
+## Get locator data from VGS .db
 query_get_loctors <- "Select SiteID, DDLong as lon, DDLat as lat, Ancestry from locator
 inner join site on site.PK_Site = Locator.FK_Site
 inner join AncestryCombinedPath ON AncestryCombinedPath.PK_Site = Site.PK_Site
 order by Ancestry, SiteID"
 
-locator_data<- dbGetQuery(mydb, query_get_loctors)
-# end VGS connection
+locator_data <- dbGetQuery(mydb, query_get_loctors)
+# End VGS connection
 dbDisconnect(mydb)
 
-# read in shape file
-shapefile_pasture<- rgdal::readOGR(dsn="E:/VGS - Deathstar II/GIS II/S_USA.Pasture", "S_USA.Pasture")
+# Read in shape file
+shapefile_pasture <- rgdal::readOGR(dsn="E:/VGS - Deathstar II/GIS II/S_USA.Pasture", "S_USA.Pasture")
 #pasture<- as.data.frame(shapefile_pasture)
 
 # Convert lat and lon to numeric
@@ -49,20 +49,22 @@ pasture_result <- over(point, shapefile_pasture)
 # Combine the results with the original data
 results_table <- cbind(locator_data, pasture_result)
 
-# create excel sheet to show results
-#View(results_table)
-write.xlsx(results_table, paste0("results/placingLocators_by_Shapefile_",Sys.Date(),".xlsx"))
+results_table <- results_table %>%
+  select("Folder Path" = Ancestry, SiteID, "Ranger District" = MANAGING_1,
+         "Allotment" = ALLOTMENT_, "Pasture" = PASTURE_NA)
 
+# Create excel sheet to show results
+write.xlsx(results_table, paste0("results/PlacingLocators_ByShapefile_",Sys.Date(),".xlsx"))
 
-
-new_data<- results_table %>% 
+# Create excel with predicited siteID based on locator
+new_data <- results_table %>% 
   mutate("PredictedName" = paste0( substr(MANAGING_O,1,2),"-",
                                    substr(MANAGING_O,3,4),"-",
                                    substr(MANAGING_O,5,6),"-",
-                                   ALLOTMENT1,"-",PASTURE_NU,"-KA#"))
+                                   ALLOTMENT1,"-",PASTURE_NU,"-##"))
 
-site_name_compare<- new_data %>% 
-  select(SiteID, PredictedName, Ancestry)
+site_name_compare <- new_data %>% 
+  select(Ancestry, SiteID, PredictedName)
 
 write.xlsx(site_name_compare, paste0("results/Projected_SiteName_Comparison_",Sys.Date(),".xlsx"))
 
